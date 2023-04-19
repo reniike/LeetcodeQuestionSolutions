66eb2787c0380c518dc1b4ef5b251905e7bda057
Babs-Olokode Aliyah <aliyaheniola91@gmail.com>
2023-04-19 23:17:39 +0100

    day six

---
+++ src/daySix/IntersectionOfArrays.java
@@ -0,0 +1,26 @@
+package daySix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectionOfArrays {
+    public static List<Integer> intersection(int[][] numbers) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < numbers[0].length; i++) {
+            int counter = 0;
+            for (int j = 1; j < numbers.length; j++) {
+                int count = 0;
+                for (int k = 0; k < numbers[j].length; k++) {
+                    if (numbers[0][i] == numbers[j][k]) count++;
+                }
+                if (count > 0) counter++;
+            }
+            if (counter == 2) result.add(numbers[0][i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intersection(new int[][]{{1, 2, 3, 4, 5}, {1, 2, 3, 4}, {3, 4, 5, 6}}));
+    }
+}
