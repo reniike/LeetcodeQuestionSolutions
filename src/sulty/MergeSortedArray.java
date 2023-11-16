57e0cdda9dda1753cfce574d888013a5c0f013b0
Babs-Olokode Aliyah <aliyaheniola91@gmail.com>
2023-11-16 23:22:52 +0100

    merge sort

--- src/sulty/MergeSortedArray.java
+++ src/sulty/MergeSortedArray.java
@@ -5,26 +5,26 @@ public class MergeSortedArray {
         int num1Length= nums1.length;
         int num2Length = nums2.length;
         int sumOfBothLength = num1Length + num2Length;
-        int[] SortedArray = new int[sumOfBothLength];
+        int[] sortedArray = new int[sumOfBothLength];
         int index=0; int count =0;
         for(int thisIndex=0;thisIndex<sumOfBothLength;thisIndex++){
             if(index<num1Length&&count<num2Length){
                 if(nums1 == null || nums1[index]>nums2[count]){
-                    SortedArray[thisIndex]=nums2[count];
+                    sortedArray[thisIndex]=nums2[count];
                     count++;
                 }
                 else{
-                    SortedArray[thisIndex]=nums1[index];
+                    sortedArray[thisIndex]=nums1[index];
                     index++;
                 }
             }
         }
         double medianValue=0;
         if(sumOfBothLength%2 == 0){
-            medianValue= (SortedArray[sumOfBothLength/2]+ SortedArray[(sumOfBothLength/2)-1])/2;
+            medianValue= (sortedArray[sumOfBothLength/2]+ sortedArray[(sumOfBothLength/2)-1])/2;
         }
         else{
-            medianValue=Math.floor(SortedArray[sumOfBothLength/2]);
+            medianValue=Math.floor(sortedArray[sumOfBothLength/2]);
         }
         return medianValue;
 
